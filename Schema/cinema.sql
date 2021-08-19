7049bf57427be06b8def5daa405064bf1fdecdd5
Timstar59 <timbalogun@hotmail.com>
2021-08-19 14:47:00 +0100

    fff

--- Schema/cinema.sql
+++ Schema/cinema.sql
@@ -4,11 +4,11 @@ CREATE DATABASE cinema;
 USE cinema;
 
 CREATE TABLE movies (
-	moviename_id VARCHAR(70) AUTO_INCREMENT,
+	movie_id INT,
 	movie_genre VARCHAR(50) NOT NULL,
 	movie_price DECIMAL NOT NULL,
     movie_rating DECIMAL NOT NULL,
-	PRIMARY KEY (moviename_id)
+	PRIMARY KEY (movie_id)
 );
 
 CREATE TABLE customer (
@@ -25,9 +25,9 @@ CREATE TABLE  film_booking (
 	movie_restrictions VARCHAR(70),
 	movie_avaliabilty DATETIME NOT NULL,
     premium_seats BOOLEAN,
-    fk_moviename_id VARCHAR(70),
+    fk_movie_id INT,
 	PRIMARY KEY (booking_price_id),
     FOREIGN KEY (fk_customer_id) REFERENCES customer(customer_id),
-    FOREIGN KEY (fk_moviename_id) REFERENCES movies(moviename_id) 	
+    FOREIGN KEY (fk_movie_id) REFERENCES movies(movie_id) 	
 );
 
